da7dbc9663069ae14f73e3b53495db0859050d6b
Jayy001 <github@skii.dev>
2024-09-21 14:00:34 +0100

    Added files

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!bin/sh
+
+# Download required binaries, libs and repos
+# Create folders and add to PATH
+# Run mount and copy libs 
+# Install debian-chroot
+# Copy kmscube to chroot
+# Install requirements in chroot
+# Build kmscube in chroot
+# Run kmscube in chroot
+
+
